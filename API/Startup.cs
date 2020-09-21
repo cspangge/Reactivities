5ff77cf422634463edfd1220b8c853531e14137d
Ran Tao <ran.tao@itic.com.au>
2020-09-22 00:10:04 +1000

    add semantic ui

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,14 @@ namespace API
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
             services.AddControllers();
         }
 
@@ -44,6 +52,8 @@ namespace API
             }
 
             // app.UseHttpsRedirection();
+
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
 
